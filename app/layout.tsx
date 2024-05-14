5828c8e50b0ac51e7e75e4b641a71df784e72f21
John <jun0205@live.com>
2024-05-15 00:38:10 +0800

    Add viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,27 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
+
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'My IP and Network Information - myip.moe',
+  description: 'My IP and Network Information',
   icons: {
     icon: '/favicon.png',
     shortcut: '/favicon.png',
     apple: '/apple-touch-icon.png',
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -16,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
