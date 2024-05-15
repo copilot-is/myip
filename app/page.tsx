df56b587b0190330d6cbe9be46e8d323028be9c9
John <jun0205@live.com>
2024-05-15 22:47:23 +0800

    Add api ui

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
               type="search"
               className="block px-3 py-2 pr-14 w-full z-20 text-lg text-gray-900 bg-gray-50 font-medium rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 outline-none"
               placeholder="Enter IP Address..."
-              defaultValue={data ? data.ip : ''}
+              defaultValue={data ? data?.ip : ''}
               required
             />
             <button
@@ -56,72 +56,85 @@ export default async function Home() {
             </button>
           </div>
         </form>
-        {data && (
-          <table className="w-full rounded-md overflow-hidden">
-            <tbody>
+        <table className="w-full rounded-lg overflow-hidden">
+          <tbody>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 w-28 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                IP
+              </td>
+              <td className="px-2 py-1">{data?.ip}</td>
+            </tr>
+            {data?.hostname && (
               <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 w-28 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  IP
+                <td className="px-2 py-1 w-24 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  Hostname
                 </td>
-                <td className="px-2 py-1">{data.ip}</td>
+                <td className="px-2 py-1">{data?.hostname}</td>
               </tr>
-              {data.hostname && (
-                <tr className="border-b-2 border-white last:border-b-0">
-                  <td className="px-2 py-1 w-24 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    Hostname
-                  </td>
-                  <td className="px-2 py-1">{data.hostname}</td>
-                </tr>
-              )}
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  City
-                </td>
-                <td className="px-2 py-1">{data.city}</td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  Region
-                </td>
-                <td className="px-2 py-1">{data.regionName}</td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  Country
-                </td>
-                <td className="px-2 py-1">{data.countryName}</td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  AS
-                </td>
-                <td className="px-2 py-1 break-words hyphens-auto">
-                  {data.as}
-                </td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  Location
-                </td>
-                <td className="px-2 py-1">
-                  {data.latitude}, {data.longitude}
-                </td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  Timezone
-                </td>
-                <td className="px-2 py-1 text-gray-800">{data.timezone}</td>
-              </tr>
-              <tr className="border-b-2 border-white last:border-b-0">
-                <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  User Agent
-                </td>
-                <td className="px-2 py-1 text-gray-800">{data.userAgent}</td>
-              </tr>
-            </tbody>
-          </table>
-        )}
+            )}
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                City
+              </td>
+              <td className="px-2 py-1">{data?.city}</td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                Region
+              </td>
+              <td className="px-2 py-1">{data?.regionName}</td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                Country
+              </td>
+              <td className="px-2 py-1">{data?.countryName}</td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                AS
+              </td>
+              <td className="px-2 py-1 break-words hyphens-auto">{data?.as}</td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                Location
+              </td>
+              <td className="px-2 py-1">
+                {data?.latitude}, {data?.longitude}
+              </td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                Timezone
+              </td>
+              <td className="px-2 py-1 text-gray-800">{data?.timezone}</td>
+            </tr>
+            <tr className="border-b-2 border-white last:border-b-0">
+              <td className="px-2 py-1 text-right text-gray-600 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                User Agent
+              </td>
+              <td className="px-2 py-1 text-gray-800">{data?.userAgent}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="w-full rounded-lg mt-3 mb-6 p-4 text-gray-800 bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <p>curl myip.moe/json</p>
+          <p>curl myip.moe/json/8.8.8.8?lang=en</p>
+        </div>
+        <footer className="text-xs text-gray-600 mb-4">
+          ©{' '}
+          <a href="https://myip.moe" className="hover:underline">
+            MyIP.moe
+          </a>
+          . All Rights Reserved.
+          <a
+            className="pl-1 hover:underline"
+            href="https://github.com/copilot-is/myip"
+          >
+            GitHub
+          </a>
+        </footer>
       </main>
     </div>
   );
