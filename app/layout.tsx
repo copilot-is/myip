bb9a69a3a5c42bf42a4a2d7acf9e3eae069e6117
John <jun0205@live.com>
2024-05-30 18:24:29 +0800

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from 'sonner';
 
 import './globals.css';
@@ -61,6 +62,7 @@ export default function RootLayout({
           </main>
         </div>
         <Toaster position="top-center" richColors />
+        <Analytics />
       </body>
     </html>
   );
