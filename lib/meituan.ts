f801ed92861f39dd26d1a33ac5ffb617a18c0ced
John <jun0205@live.com>
2025-08-15 22:12:26 +0800

    no message

--- lib/meituan.ts
+++ lib/meituan.ts
@@ -94,7 +94,7 @@ const getMeituanLocationByIP = async (
         'User-Agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
         'Accept-Language': 'zh-CN,zh;q=0.9',
-        Accept: 'application/json',
+        Accept: 'application/json;charset=UTF-8',
         Referer: 'https://www.meituan.com',
         Origin: 'https://www.meituan.com'
       }
